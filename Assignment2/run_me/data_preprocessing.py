52af1d65ffd81be42a0183365584fe73f9f00ffc
kkhawk20 <kehawkins@chapman.edu>
2024-03-01 13:11:04 -0800

    Data Preprocessing, fixes, etc

--- Assignment2/run_me/data_preprocessing.py
+++ Assignment2/run_me/data_preprocessing.py
@@ -1,53 +1,58 @@
 # data_preprocessing.py
 # Purpose is to load in TTV data, augment training set
 
-import matplotlib.pyplot as plt
+
 import os
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import Sequence
+import numpy as np
+import cv2
 import warnings
 warnings.filterwarnings('ignore')
 
 def load_data():
-    # Ensuring the data is being accessed! 
+    # Ensuring the data is being accessed!
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    data_dir = os.path.join(base_dir, 'Data')
+    data_dir = os.path.join(base_dir, 'Data_new')
 
-    train_dir = os.path.join(data_dir, 'Train')
-    test_dir = os.path.join(data_dir, 'Test')
-    val_dir = os.path.join(data_dir, 'Val')
+    # Define the directories for images and masks
+    train_image_dir = os.path.join(data_dir, 'Train', 'Images')
+    train_mask_dir = os.path.join(data_dir, 'Train', 'Masks')
+    val_image_dir = os.path.join(data_dir, 'Val', 'Images')
+    val_mask_dir = os.path.join(data_dir, 'Val', 'Masks')
+    test_image_dir = os.path.join(data_dir, 'Test', 'Images')
+    test_mask_dir = os.path.join(data_dir, 'Test', 'Masks')
 
     # Pre-setting the known image size and set batch size
-    image_size = (256,256)
+    image_size = (256, 256)
     batch_size = 32
 
-    # Adding preprocessing for augmentation!! 
-    # Increasing diversity, etc. 
-    trData = ImageDataGenerator(
-        rotation_range = 180,
-        width_shift_range = 0.2,
-        height_shift_range = 0.2,
-        horizontal_flip = True,
-        zoom_range = 0.2,
-        fill_mode = 'nearest'
-    )
-    # Reading in training DS with categorical classes
-    train_ds = trData.flow_from_directory(directory = train_dir, 
-                                        target_size = image_size, 
-                                        class_mode = 'categorical', 
-                                        batch_size = batch_size)
+    # Create instances of the SegmentationDataGenerator for each dataset
+    train_gen = SegmentationDataGenerator(train_image_dir, train_mask_dir, batch_size, image_size)
+    val_gen = SegmentationDataGenerator(val_image_dir, val_mask_dir, batch_size, image_size)
+    test_gen = SegmentationDataGenerator(test_image_dir, test_mask_dir, batch_size, image_size)
 
-    # Reading in testing DS with categorical classes
-    teData = ImageDataGenerator()
-    test_ds = teData.flow_from_directory(directory = test_dir, 
-                                        target_size = image_size, 
-                                        class_mode = 'categorical', 
-                                        batch_size = batch_size)
+    return train_gen, val_gen, test_gen
 
-    # Reading in validation DS with categorical classes
-    valData = ImageDataGenerator()
-    val_ds = valData.flow_from_directory(directory = val_dir, 
-                                        target_size = image_size, 
-                                        class_mode = 'categorical', 
-                                        batch_size = batch_size)
+class SegmentationDataGenerator(Sequence):
+    def __init__(self, image_dir, mask_dir, batch_size, image_size):
+        self.image_paths = [os.path.join(image_dir, img) for img in os.listdir(image_dir)]
+        self.mask_paths = [os.path.join(mask_dir, mask) for mask in os.listdir(mask_dir)]
+        self.batch_size = batch_size
+        self.image_size = image_size
 
-    return train_ds, test_ds, val_ds
+    def __len__(self):
+        return len(self.image_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        batch_image_paths = self.image_paths[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_mask_paths = self.mask_paths[idx * self.batch_size:(idx + 1) * self.batch_size]
+        
+        batch_images = np.array([cv2.resize(cv2.imread(file_path), self.image_size) for file_path in batch_image_paths])
+        batch_masks = np.array([cv2.resize(cv2.imread(file_path, cv2.IMREAD_GRAYSCALE), self.image_size) for file_path in batch_mask_paths])
+        
+        # Normalization and expansion of dimensions might be needed depending on how your model expects the data
+        batch_images = batch_images / 255.0
+        batch_masks = np.expand_dims(batch_masks, -1) / 255.0
+        
+        return batch_images, batch_masks
+
