a1f7064f4984cfeea4e08765bfcf1964e383c5a8
kkhawk20 <kehawkins@chapman.edu>
2024-03-01 12:33:14 -0800

    Tuning fixes

--- Assignment2/run_me/utilities.py
+++ Assignment2/run_me/utilities.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 from tensorflow.keras.preprocessing.image import load_img
 from keras.models import load_model
-import cv2
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg16 import preprocess_input
@@ -14,7 +13,6 @@ import matplotlib.cm as cm
 import warnings
 warnings.filterwarnings('ignore')
 
-## Implementing GradCAM
 def get_img_path(base_dir):
     category = random.choice(['Fresh', 'Rotten'])  # Choose between 'fresh' or 'rotten'
     img_dir = os.path.join(base_dir, category)
@@ -27,89 +25,6 @@ def load_and_preprocess_image(img_path):
     img_array_expanded = np.expand_dims(img_array, axis=0)
     return preprocess_input(img_array_expanded)
 
-def make_gradcam_heatmap(img_array, model, last_conv_layer_name, classifier_layer_names, pred_index=None):
-    # First, we create a model that maps the input image to the activations
-    # of the last conv layer as well as the output predictions
-
-    last_conv_layer = model.get_layer('vgg16').get_layer(last_conv_layer_name)
-    last_conv_layer_model = Model(inputs = model.get_layer('vgg16').input,
-                                outputs = last_conv_layer.output)
-
-    # output_file_path = os.path.join(os.path.join(os.path.dirname(__file__), 'outputs'), 'VGGmodel_summary.txt')
-    # with open(output_file_path, 'w') as f:
-    #     def print_to_file(text):
-    #         print(text, file=f)
-    #     # Print the vgg16 summary to file to troubleshooot
-    #     last_conv_layer.summary(print_fn=print_to_file)
-
-    # Now, we create a model that maps the activations of the last conv
-    # layer to the final class predictions
-    classifier_input = tf.keras.Input(shape=last_conv_layer.output.shape[1:])
-    x = classifier_input
-    for layer_name in classifier_layer_names:
-        x = model.get_layer(layer_name)(x)
-    classifier_model = Model(classifier_input, x)
-
-    # Then, we compute the gradient of the top predicted class for our input image
-    # with respect to the activations of the last conv layer
-    with tf.GradientTape() as tape:
-        # Compute activations of the last conv layer and make the predictions
-        last_conv_layer_output = last_conv_layer_model(img_array)
-        tape.watch(last_conv_layer_output)
-        preds = classifier_model(last_conv_layer_output)
-        if pred_index is None:
-            pred_index = tf.argmax(preds[0])
-        top_class_channel = preds[:, pred_index]
-
-    # This is the gradient of the top predicted class with regard to
-    # the output feature map of the last conv layer
-    grads = tape.gradient(top_class_channel, last_conv_layer_output)
-
-    # This is a vector where each entry is the mean intensity of the gradient
-    # over a specific feature map channel
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-
-    # Multiply each channel in the feature map array by 'how important this channel is'
-    # with regard to the top predicted class
-    last_conv_layer_output = last_conv_layer_output.numpy()[0]
-    pooled_grads = pooled_grads.numpy()
-    for i in range(pooled_grads.shape[-1]):
-        last_conv_layer_output[:, :, i] *= pooled_grads[i]
-
-    # The channel-wise mean of the resulting feature map
-    # is our heatmap of class activation
-    heatmap = np.mean(last_conv_layer_output, axis=-1)
-
-    # For visualization purpose, we will also normalize the heatmap between 0 & 1
-    heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
-    return heatmap
-
-def save_and_display_gradcam(img_path, heatmap, cam_path, alpha=0.4):
-    # Load the original image
-    img = tf.keras.preprocessing.image.load_img(img_path)
-    img = tf.keras.preprocessing.image.img_to_array(img)
-
-    # Rescale heatmap to a range 0-255
-    heatmap = np.uint8(255 * heatmap)
-
-    # Use jet colormap to colorize heatmap
-    jet = cm.get_cmap("jet")
-
-    # Use RGB values of the colormap
-    jet_colors = jet(np.arange(256))[:, :3]
-    jet_heatmap = jet_colors[heatmap]
-
-    # Create an image with RGB colorized heatmap
-    jet_heatmap = tf.keras.preprocessing.image.array_to_img(jet_heatmap)
-    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
-    jet_heatmap = tf.keras.preprocessing.image.img_to_array(jet_heatmap)
-
-    # Superimpose the heatmap on original image
-    superimposed_img = jet_heatmap * alpha + img
-    superimposed_img = tf.keras.preprocessing.image.array_to_img(superimposed_img)
-
-    # Save the superimposed image
-    superimposed_img.save(cam_path)
 
 def model_eval(hist):
 
@@ -117,7 +32,7 @@ def model_eval(hist):
     base_dir = os.path.dirname(os.path.realpath(__file__))
     data_dir = os.path.join(base_dir, 'Data')
     test_dir = os.path.join(data_dir, 'Test')
-    image_size = (244,244)
+    image_size = (256,256)
     
     if not os.path.exists(save_dir):
         print("Unable to save output images/files")
@@ -125,19 +40,6 @@ def model_eval(hist):
     # Creating visuals to analyze performance of model
     # Starting with accuracy vs loss for test/validation
     # Moving to visualizing 5 images from test set and predictions
-
-    # Plotting the validation accuracy & loss vs training accuracy & loss
-    plt.figure()
-    # plt.plot(hist.history['accuracy'])
-    plt.plot(hist.history['val_accuracy'])
-    # plt.plot(hist.history['loss'])
-    plt.plot(hist.history['val_loss'])
-    plt.title("Model Accuracy")
-    plt.ylabel("Accuracy")
-    plt.xlabel("Epoch")
-    plt.legend(["Validation Accuracy","Validation Loss"])
-    plt.savefig(os.path.join(save_dir, 'Accuracy_Loss1.jpg'))
-    plt.close()
 
     # Plotting v2 to see deeper analysis...
     plt.figure(figsize=(10, 5))
@@ -215,46 +117,3 @@ def model_eval(hist):
 
     # Save the grid image! s
     grid_image.save(os.path.join(save_dir, 'test_predictions_grid.jpg'))
-
-    # 5 images with GRADCAM overlay! 
-    for index, filename in enumerate(selected_images):
-        # Load and process each image
-        img_path = os.path.join(test_dir, filename)
-        img_array = load_and_preprocess_image(img_path)  # Make sure this matches your preprocessing
-        img = load_img(img_path, target_size=image_size)
-        # Make predictions
-        output = saved_model.predict(img_array)
-        prediction = "Fresh" if output[0][0] > output[0][1] else "Rotten"
-
-        # Generate Grad-CAM heatmap
-        heatmap = make_gradcam_heatmap(img_array, saved_model, last_conv_layer_name, classifier_layer_names)
-
-        # Overlay heatmap on original image
-        overlay_image_path = os.path.join(save_dir, f'overlay_{index}.jpg')  # Example path for overlay image
-        save_and_display_gradcam(img_path, heatmap, overlay_image_path, alpha=0.4)
-        overlay_img = Image.open(overlay_image_path)  # Load the overlay image
-
-        # Overlay the prediction on the image
-        img = img.convert("RGB")
-        draw = ImageDraw.Draw(overlay_img)
-        text_color = (255, 255, 255)  # White text color
-        text_background = (0, 0, 0)  # Black background for text
-        margin = 10
-        text_width, text_height = draw.textsize(prediction, font=font)
-        draw.rectangle([(0, img.height - text_height - 2 * margin), 
-                        (text_width + 2 * margin, img.height)], 
-                    fill=text_background)
-        draw.text((margin, img.height - text_height - margin), prediction, font=font, fill=text_color)
-
-        # Calculate the position of the image in the grid and paste
-        x = index % grid_size[0] * image_size[0]
-        y = index // grid_size[0] * image_size[1]
-        grid_image.paste(overlay_img, (x, y))
-    
-    grid_image.save(os.path.join(save_dir, 'GRADCAMtest_predictions_grid.jpg'))
-
-    # GRAD-CAM IMPLEMENTATION
-    # Assuming `model` is your full model with VGG16 as the base and custom dense layers on top
-    img_array = load_and_preprocess_image(img_path)  # Assuming you've already defined this function
-    heatmap = make_gradcam_heatmap(img_array, saved_model, last_conv_layer_name, classifier_layer_names)
-    save_and_display_gradcam(img_path, heatmap, cam_path=os.path.join(save_dir, 'gradCAM.jpg'))  # Update save path
