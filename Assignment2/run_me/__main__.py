543b11c7f8085cdd8c61e7eebc60c30740a8287e
kkhawk20 <kehawkins@chapman.edu>
2024-03-01 13:17:17 -0800

    Changes to run!

--- Assignment2/run_me/__main__.py
+++ Assignment2/run_me/__main__.py
@@ -4,9 +4,14 @@
 from . import *
 def main():
     print("Running Image Segmentation Model!! ...")
-    data = load_data()
-    train = unet(data)
-    util = model_eval(train)
+    # Load the data using the custom SegmentationDataGenerator
+    train_gen, val_gen, test_gen = load_data()
+    
+    # Train the U-Net model with the training and validation generators
+    model, history = unet(train_gen=train_gen, val_gen=val_gen)
+    
+    # Evaluate the model using the test generator and calculate metrics
+    model_eval(model=model, test_gen=test_gen)
 
 if __name__ == '__main__':
     main()
