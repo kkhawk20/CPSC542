ba0024bf208cf3adea3eda49cda5d045a4e746dc
kkhawk20 <kehawkins@chapman.edu>
2024-03-12 14:18:36 -0700

    UGH

--- Assignment2/run_me/tuners.py
+++ Assignment2/run_me/tuners.py
@@ -5,6 +5,7 @@ from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.applications import ResNet50
 from keras_tuner import Objective
 import tensorflow as tf
 import keras_tuner as kt
@@ -33,6 +34,66 @@ def bce_dice_loss(y_true, y_pred):
     bce = BinaryCrossentropy(from_logits=False)
     return bce(y_true, y_pred) + dice_loss(y_true, y_pred)
 
+# Testing out a different pre-trained encoder architecture
+def build_resnet(hp):
+    input_size=(256,256,3)
+    inputs = Input(input_size)
+    encoder = ResNet50(include_top = False, weights = 'imagenet',
+                       input_tensor = inputs)
+
+    for layer in encoder.layers[:15]: # Freeze top 15 layers (leave 4)
+        layer.trainable = False
+
+    for i, layer in enumerate(encoder.layers):
+        print(i, layer.name, layer.trainable)
+
+    # Encoder 
+    skip_connections = ['conv2_block3_out', 'conv3_block4_out', 'conv4_block6_out', 'conv5_block3_out']
+    encoder_outputs = [encoder.get_layer(name).output for name in skip_connections]
+    c1, c2, c3, c4 = encoder_outputs
+
+    # Bottleneck
+    bottleneck = c4
+    
+    # Bottleneck
+    bottleneck = c4
+
+    # Decoder
+    up6 = Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(bottleneck)
+    merge6 = concatenate([c4, up6], axis=3)
+    c6 = Conv2D(256, 3, activation='relu', padding='same')(merge6)
+    c6 = Conv2D(256, 3, activation='relu', padding='same')(c6)
+
+    up7 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(c6)
+    merge7 = concatenate([c3, up7], axis=3)
+    c7 = Conv2D(128, 3, activation='relu', padding='same')(merge7)
+    c7 = Conv2D(128, 3, activation='relu', padding='same')(c7)
+
+    up8 = Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(c7)
+    merge8 = concatenate([c2, up8], axis=3)
+    c8 = Conv2D(64, 3, activation='relu', padding='same')(merge8)
+    c8 = Conv2D(64, 3, activation='relu', padding='same')(c8)
+
+    up9 = Conv2DTranspose(32, 2, strides=(2, 2), padding='same')(c8)
+    merge9 = concatenate([c1, up9], axis=3)
+    c9 = Conv2D(32, 3, activation='relu', padding='same')(merge9)
+    c9 = Conv2D(32, 3, activation='relu', padding='same')(c9)
+    
+    outputs = Conv2D(1, 1, activation = 'sigmoid')(c9)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    
+    # Done with model, compile now
+    lr = hp.Choice('learning_rate', values = [1e-3, 1e-4, 1e-5])
+
+    model.compile(optimizer = Adam(learning_rate = lr), 
+                loss = bce_dice_loss, 
+                metrics = [dice_coeff])
+    
+    return model
+
+# This is a vgg16 model as the encoder for the u-net
+# Kinda sucks, trying other models now...
 def build_model(hp):
     input_size=(256,256,3)
 
@@ -79,7 +140,6 @@ def build_model(hp):
 
     model = Model(inputs=vgg16.input, outputs=outputs)
     
-
     # Done with model, compile now
     lr = hp.Choice('learning_rate', values = [1e-3, 1e-4, 1e-5])
 
